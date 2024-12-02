bbbf1935523738335d877e62779cb996d29e9082
carl <carlos.acitores@colba.es>
2024-12-02 13:08:42 +0100

    setup to run day

---
+++ 2024/src/day1/main.rs
@@ -0,0 +1,3 @@
+pub fn run_day1() {
+	println!("day 1")
+}
--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+mod day1;
+use crate::day1::main::run_day1;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let day_number = args[2].as_str();
+    match day_number {
+        "1" => run_day1(),
+        _ => println!("not worked on this day")
+    }
 }
