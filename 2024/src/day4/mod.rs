d55d9feac2a82f1ce02e4856fc07bb86a0fe39a8
carl <carlos.acitores@colba.es>
2024-12-21 14:23:40 +0100

    day 4 part 1 completed

--- 2024/src/day4/mod.rs
+++ 2024/src/day4/mod.rs
@@ -1,3 +1,7 @@
+mod part1;
+
 pub fn run() {
-  println!("day 4")
+  println!("day 4");
+  part1::run_example();
+  part1::run_complete();
 }
---
+++ 2024/src/day4/part1.rs
@@ -0,0 +1,137 @@
+use std::fs;
+use strum::{EnumIter, IntoEnumIterator};
+
+pub fn run_example() {
+  println!("executing example PART 1");
+  match fs::read_to_string("./src/day4/example_input_part1.txt") {
+    Ok(input) => { 
+			let result = execute_part1(input);
+			println!("result should be 18 -> {} | {}", result, result == 18)
+		},
+		Err(reading_file_error) => {
+			println!("bad ): - {}", reading_file_error.to_string());
+			panic!("PANIC");
+		}
+  }
+}
+
+pub fn run_complete() {
+  println!("executing complete PART 1");
+  match fs::read_to_string("./src/day4/complete_input_part1.txt") {
+    Ok(input) => { 
+			let result = execute_part1(input);
+			println!("result is [{}]", result); // 2547
+		},
+		Err(reading_file_error) => {
+			println!("bad ): - {}", reading_file_error.to_string());
+			panic!("PANIC");
+		}
+  } 
+}
+
+fn execute_part1(input: String) -> i32 {
+  let letters = read_letters(input);
+  letters
+    .iter()
+    .fold(0, |total, current_letter| 
+      total + process_letter(
+        current_letter, 
+        &letters, 
+      )
+    )
+}
+
+fn read_letters(input: String) -> Vec<Letter> {
+  let binding = input
+    .split("\n")
+    .collect::<Vec<&str>>();
+  let lines = binding
+    .iter()
+    .enumerate();
+  let mut letters = vec![];
+
+  for (j, line) in lines {
+    for (i, charact) in line.chars().into_iter().enumerate() {
+      letters.push(Letter {
+        position: (
+          i.try_into().unwrap(),
+          j.try_into().unwrap()
+        ),
+        value: charact
+      });
+    }
+  }
+
+  letters
+}
+
+fn process_letter(
+  current_letter: &Letter, 
+  letters: &Vec<Letter>
+) -> i32 {
+  if current_letter.value != 'X' {
+    return 0;
+  }
+
+  let mut total_matches = 0;
+
+  for direction in Directions::iter() {
+    let m_letter = letters
+      .iter()
+      .find(|letter| move_in_direction(&direction, current_letter.position, 1) == letter.position)
+      .is_some_and(|letter| letter.value == 'M');
+    let a_letter = letters
+      .iter()
+      .find(|letter| move_in_direction(&direction, current_letter.position, 2) == letter.position)
+      .is_some_and(|letter| letter.value == 'A');
+    let s_letter = letters
+      .iter()
+      .find(|letter| move_in_direction(&direction, current_letter.position, 3) == letter.position)
+      .is_some_and(|letter| letter.value == 'S');
+
+    let is_match = m_letter & a_letter & s_letter;
+    total_matches = if is_match {
+      total_matches +1
+    } else {
+      total_matches
+    }
+
+  }
+
+  total_matches
+}
+
+fn move_in_direction(direction: &Directions, position: (i32, i32), multiplier: i32) -> (i32, i32) {
+  let movement_vector = match direction {
+    Directions::Up        => (0 * multiplier,  1 * multiplier),
+    Directions::Down      => (0 * multiplier,  -1 * multiplier),
+    Directions::Right     => (1 * multiplier,  0 * multiplier),
+    Directions::Left      => (-1 * multiplier, 0 * multiplier),
+    Directions::UpRight   => (1 * multiplier,  1 * multiplier),
+    Directions::UpLeft    => (-1 * multiplier,  1 * multiplier),
+    Directions::DownRight => (1 * multiplier,  -1 * multiplier),
+    Directions::DownLeft  => (-1 * multiplier,  -1 * multiplier),
+  };
+  (
+    movement_vector.0 + position.0,
+    movement_vector.1 + position.1
+  )
+}
+
+#[derive(Clone)]
+struct Letter {
+  position: (i32, i32),
+  value: char,
+}
+
+#[derive(Debug, EnumIter)]
+enum Directions {
+  Up,
+  Down,
+  Right,
+  Left,
+  UpRight,
+  UpLeft,
+  DownRight,
+  DownLeft
+}
